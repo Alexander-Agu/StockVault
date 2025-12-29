a0bb1270f9c9b1d62b26b1f964b97517f5d6cde4
Alexander-Agu <firstsonagu@gmail.com>
2025-12-29 15:01:37 +0200

    initializing frontend

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class", // enables dark mode via class="dark"
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+      },
+      colors: {
+      },
+    },
+  },
+  plugins: [],
+};
