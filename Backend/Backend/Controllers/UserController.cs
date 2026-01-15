20f8aadcb979e566aa67a9e4ca78c197770be36d
Alexander-Agu <firstsonagu@gmail.com>
2026-01-15 14:54:13 +0200

    Created the login and logout endpoints

--- Backend/Backend/Controllers/UserController.cs
+++ Backend/Backend/Controllers/UserController.cs
@@ -22,10 +22,34 @@ namespace Backend.Controllers
 
         
         // Verifies user's email
-        [HttpPost("verify")]
+        [HttpPut("verify-email")]
         public async Task<ActionResult<Dictionary<string, object>>> VerifyUserEmailAsync([FromQuery] string email, [FromQuery] string otp)
         {
             var response = await userService.VerifyEmailAsync(email, otp);
+
+            if (response["result"] == "Error") return BadRequest(response);
+
+            return Ok(response);
+        }
+
+        
+        // Log's user into their account
+        [HttpGet("login")]
+        public async Task<ActionResult<Dictionary<string, object>>> LoginUserAsync([FromBody] LoginDto loginDetails)
+        {
+            var response = await userService.LoginAsync(loginDetails);
+
+            if (response["result"] == "Error") return BadRequest(response);
+
+            return Ok(response);
+        }
+
+        
+        // Log's user out of their account
+        [HttpPut("logout/{userId}")]
+        public async Task<ActionResult<Dictionary<string, object>>> LogoutUserAsync(int userId)
+        {
+            var response = await userService.LogoutAsync(userId);
 
             if (response["result"] == "Error") return BadRequest(response);
 
