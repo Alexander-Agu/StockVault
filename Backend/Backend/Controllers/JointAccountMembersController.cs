1b43a8bd53ab94cf24692a173986dda820c81f0b
Hamzah <hashaikjhb024@student.wethinkcode.co.za>
2026-03-09 21:55:39 +0200

    Add classes for JointAccountMembers

---
+++ Backend/Backend/Controllers/JointAccountMembersController.cs
@@ -0,0 +1,81 @@
+using System.Security.Claims;
+using System.Security.Principal;
+using Backend.Dtos.AccountDtos;
+using Backend.Dtos.AccountLockDtos;
+using Backend.Dtos.JointAccountDtos;
+using Backend.Dtos.PersonalAccountDtos;
+using Backend.Dtos.ResponseDto;
+using Backend.Services.JointAccountService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class JointAccountMembersController(IJointAccountService accountService) : Controller
+    {
+        // Allows user's to create a joint account
+        [HttpPost("")]
+        public async Task<ActionResult> CreateJointAccount([FromBody] CreateAccountDto newAccount)
+        {
+            int userId = GetUserIdFromClaims();
+            ApiResponse<JointAccountDto>? response = await accountService.CreateJointAccountAsync(userId, newAccount);
+
+            return HandleResponse(response);
+        }
+
+        [HttpGet("{accountId}")]
+        public async Task<ActionResult> GetJointAccountAsync(int accountId)
+        {
+            int userId = GetUserIdFromClaims();
+            ApiResponse<JointAccountDto>? response = await accountService.GetJointAccountAsync(userId, accountId);
+
+            return HandleResponse(response);
+        }
+
+        [HttpGet("")]
+        public async Task<ActionResult> GetAllJointAccountAsync()
+        {
+            int userId = GetUserIdFromClaims();
+            ApiResponse<List<JointAccountDto>>? response = await accountService.GetAllJointAccountsAsync(userId);
+
+            return HandleResponse(response);
+        }
+
+        [HttpDelete("{accountId}")]
+        public async Task<ActionResult> DeleteJointAccountAsync(int accountId)
+        {
+            int userId = GetUserIdFromClaims();
+            ApiResponse<JointAccountDto>? response = await accountService.DeleteJointAccountAsync(userId, accountId);
+
+            return HandleResponse(response);
+        }
+
+        private int GetUserIdFromClaims()
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+            if (userIdClaim == null || !int.TryParse(userIdClaim.Value, out int userId))
+            {
+                throw new UnauthorizedAccessException("User ID not found in token");
+            }
+            return userId;
+        }
+
+        protected ActionResult HandleResponse<T>(ApiResponse<T> response)
+        {
+            return response.ResponseCode switch
+            {
+                ResponseCode.Ok => Ok(response),
+                ResponseCode.Created => StatusCode(StatusCodes.Status201Created, response),
+                ResponseCode.BadRequest => BadRequest(response),
+                ResponseCode.NotFound => NotFound(response),
+                ResponseCode.Unauthorized => Unauthorized(),
+                ResponseCode.Forbidden => Forbid(),
+                ResponseCode.ServerError => StatusCode(StatusCodes.Status500InternalServerError, response),
+                _ => BadRequest(response)
+            };
+        }
+    }
+}
---
+++ Backend/Backend/Repository/JointAccountMembersRepository/IJointAccountMembersRepository.cs
@@ -0,0 +1,62 @@
+using Backend.Dtos.JointAccountDtos;
+using Backend.Dtos.PersonalAccountDtos;
+using Backend.Entities;
+
+namespace Backend.Repository.JointAccountRepository
+{
+    public interface IJointAccountRepository
+    {
+        /*
+         * TODO: Save database changes
+         */
+        public Task SaveChangesAsync();
+          
+
+        /*
+         * TODO: New joint account will be saved to the database
+         */
+        public Task AddJointAccountAsync(JointAccount newAccount);  
+
+
+        /*
+         * TODO: Fetches all personal account
+         */
+        public Task<List<PersonalAccount>> GetAllPersonalAccountsAsync(int userId);
+
+
+        /*
+         * TODO: Fetches all joint accounts
+         */
+        public Task<List<JointAccountDto>> GetAllJointTableAccountsAsync(int userId);
+
+
+        /*
+         * TODO: Fetches a personal account by userId and personalId
+         */
+        public Task<PersonalAccount> GetPersonalAccountByIdAsync(int userId, int accountId);
+
+
+        /*
+         * TODO: Fetches a joint account by userId and accountId
+         */
+        public Task<JointAccountDto> GetJointTableAccountByIdAsync(int userId, int accountId);
+
+
+        /*
+         * TODO: Fetches a joint account entity by userId and accountId
+         */
+        public Task<JointAccount> GetJointAccountByIdAsync(int userId, int accountId);
+
+
+        /*
+         * TODO: Allows a user to delete a joint account
+         */
+        public Task DeleteJointAccountByIdAsync(int userId, int accountId);
+
+
+        /*
+         * TODO: Allows user to check if joint account exists
+         */
+        public Task<bool> JointAccountExist(int userId, string title);
+    }
+}
